5c733cfffcdd68a1cc52e4f6074e2b8dc5d204c1
Einzigart <faridhakim2@gmail.com>
2023-12-18 10:25:59 +0700

    nav n table adjust

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
      <link rel ="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
+     <link rel ="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
 </head>
 <body>
      <nav>
@@ -14,11 +15,8 @@
             <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" width="40px" height="40px" />
             <h3>Universitas Sebelas Maret</h3>
         </div>
-          <div class="userInfo">
-               <img src="{{ asset('assets/images/user.svg') }}" width="20px" heigh="20px"/>
-               <h3>{{session("nama")}}</h3>
-          </div>
-          <div class="menu">
+        
+        <div class="menu">
                @if(session("role_id") == 1)
                <a href='{{url('site/daftar-mk-tayang')}}'><h3>Daftar MK Tayang</h3></a>
                <a href='{{url('site/daftar-mahasiswa')}}'><h3>Daftar Mahasiswa</h3></a>
@@ -38,13 +36,19 @@
                <a href='{{url('site/lihat-nilai',[session('nim_nip')])}}'><h3>Lihat Nilai</h3></a>
                <a href='{{url('site/ambil-mk',[session('nim_nip')])}}'><h3>Ambil MK</h3></a>
                @endif
-               
+
           </div>
+
           <div class="foot">
-            <!-- Logout with an icon -->
-            <a href="{{ url('logout') }}"><img src="{{ asset('assets/images/logout-icon.png') }}" alt="Logout" width="20px" height="20px" /></a>
+               <div class="userInfo">
+                    <img src="{{ asset('assets/images/user.svg') }}" width="20px" heigh="20px"/>
+                    <h3>{{session("nama")}}</h3>
+               </div>
+               <!-- Logout with an icon -->
+               <a href="{{ url('logout') }}"><img src="{{ asset('assets/images/logout-icon.png') }}" alt="Logout" width="20px" height="20px" /></a>
           </div>     
      </nav>
+
      <main>
           <div class="dashboard">
                <h1>Selamat Datang <br/>{{ session("nama")}}</h1>
